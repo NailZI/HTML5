eef79e78a063d55b9af40ef1338d8e7766947d84
NailZI <duble76@gmail.com>
2022-01-28 10:55:24 +0300

    add playlist.js

---
+++ playlist.js
@@ -0,0 +1,10 @@
+window.onload = init;
+
+function init() {
+    let button = document.getElementById("addButton")
+    button.onclick = handleButtonClick
+}
+
+function handleButtonClick() {
+    alert("Button was clicked!")
+}
