df15132c488dc7cfab1160ec032d63266458a029
NailZI <duble76@gmail.com>
2022-01-28 15:17:00 +0300

    page  157

--- playlist.js
+++ playlist.js
@@ -3,8 +3,21 @@ window.onload = init;
 function init() {
     let button = document.getElementById("addButton")
     button.onclick = handleButtonClick
+    loadPlayList()
 }
 
 function handleButtonClick() {
-    alert("Button was clicked!")
-}
+    //alert("Button was clicked!")
+    let textInput = document.getElementById("songTextInput")
+    let songName = textInput.value
+    // if (songName =='') {
+    //     alert('Please enter a song')
+    // } else {
+    //     alert('Adding ' + songName)
+    // }
+    let li = document.createElement("li")
+    li.innerHTML = songName
+    let ul = document.getElementById("playlist")
+    ul.appendChild(li)
+    save(songName)
+}   
