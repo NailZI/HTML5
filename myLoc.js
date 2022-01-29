86dea103e4198b3ed99ae28eddfe9e51ace93925
NailZI <duble76@gmail.com>
2022-01-29 18:48:13 +0300

    page 220\

--- myLoc.js
+++ myLoc.js
@@ -1,10 +1,10 @@
-const newLocal = 47.624851;
-var ourCoords =  {
-	latitude:   newLocal,
-	longitude: -122.52099
+var ourCoords = {
+  latitude: 47.624851,
+  longitude: -122.52099,
 };
 
 window.onload = getMyLocation();
+
 function getMyLocation() {
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(displayLocation, displayError);
@@ -20,57 +20,71 @@ function displayLocation(position) {
   let div = document.getElementById("location");
   div.innerHTML = "Ваша Широта: " + latitude + ", Долгота: " + longitude;
 
-  let km = computeDistance(position.coords, ourCoords)
-  let distance = document.getElementById("distance")
-  distance.innerHTML = "Вы в " + km + " км от WickedlySmart HQ"
+  let km = computeDistance(position.coords, ourCoords);
+  let distance = document.getElementById("distance");
+  distance.innerHTML = "Вы в " + km + " км от WickedlySmart HQ";
+
+  showMap(position.coords);
 }
 
 function displayError(error) {
-    let errorTypes = {
-        0: "Неизвестная ошибка",
-        1: "Permission denied by user",
-        2: "Position is not available",
-        3: "Request timed out"
-    }
-    let errorMessage = errorTypes[error.code]
-    if (error.code == 0 || error.code == 2) {
-        errorMessage = errorMessage + " " + error.message
-    }
-    let div =document.getElementById("location")
-    div.innerHTML =errorMessage
+  let errorTypes = {
+    0: "Неизвестная ошибка",
+    1: "Permission denied by user",
+    2: "Position is not available",
+    3: "Request timed out",
+  };
+  let errorMessage = errorTypes[error.code];
+  if (error.code == 0 || error.code == 2) {
+    errorMessage = errorMessage + " " + error.message;
+  }
+  let div = document.getElementById("location");
+  div.innerHTML = errorMessage;
 }
 
 function computeDistance(startCoords, destCoords) {
-	var startLatRads = degreesToRadians(startCoords.latitude);
-	var startLongRads = degreesToRadians(startCoords.longitude);
-	var destLatRads = degreesToRadians(destCoords.latitude);
-	var destLongRads = degreesToRadians(destCoords.longitude);
+  var startLatRads = degreesToRadians(startCoords.latitude);
+  var startLongRads = degreesToRadians(startCoords.longitude);
+  var destLatRads = degreesToRadians(destCoords.latitude);
+  var destLongRads = degreesToRadians(destCoords.longitude);
 
-	var Radius = 6371; // radius of the Earth in km
-	var distance = Math.acos(Math.sin(startLatRads) * Math.sin(destLatRads) + 
-					Math.cos(startLatRads) * Math.cos(destLatRads) *
-					Math.cos(startLongRads - destLongRads)) * Radius;
+  var Radius = 6371; // radius of the Earth in km
+  var distance =
+    Math.acos(
+      Math.sin(startLatRads) * Math.sin(destLatRads) +
+        Math.cos(startLatRads) *
+          Math.cos(destLatRads) *
+          Math.cos(startLongRads - destLongRads)
+    ) * Radius;
 
-	return distance;
+  return distance;
 }
 
 function degreesToRadians(degrees) {
-	radians = (degrees * Math.PI)/180;
-	return radians;
+  radians = (degrees * Math.PI) / 180;
+  return radians;
 }
 
-function showMap(coords) {
-	var googleLatAndLong = new google.maps.LatLng(coords.latitude, 
-												  coords.longitude);
-	var mapOptions = {
-		zoom: 10,
-		center: googleLatAndLong,
-		mapTypeId: google.maps.MapTypeId.ROADMAP
-	};
-	var mapDiv = document.getElementById("map");
-	map = new google.maps.Map(mapDiv, mapOptions);
+// let googleLatAndLong = new google.maps.LatLng(latitude, longitude);
+// let mapOptions = {
+//   zoom: 10,
+//   center: googleLatAndLong,
+//   mapTypeId: google.maps.MapTypeID.ROADMAP,
+// };
+// let map;
 
-	// add the user marker
-	var title = "Your Location";
-	var content = "You are here: " + coords.latitude + ", " + coords.longitude;
-	addMarker(map, googleLatAndLong, title, content);
+function showMap(coords) {
+  var googleLatAndLong = new google.maps.LatLng(coords.latitude,
+                          coords.longitude);
+  var mapOptions = {
+    zoom: 21,
+    center: googleLatAndLong,
+    mapTypeId: google.maps.MapTypeId.Satellite};
+  var mapDiv = document.getElementById("map");
+  map = new google.maps.Map(mapDiv, mapOptions);
+}
+
+	// // add the user marker
+	// var title = "Your Location";
+	// var content = "You are here: " + coords.latitude + ", " + coords.longitude;
+	// addMarker(map, googleLatAndLong, title, content);
