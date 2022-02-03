22b2154defc79c66b371def16b38a2aae7b98aaf
NailZI <duble76@gmail.com>
2022-02-03 14:56:23 +0300

    page 382

--- tweetshirt.js
+++ tweetshirt.js
@@ -7,6 +7,7 @@ function previewHandler() {
     let canvas = document.getElementById("tshirtCanvas")
     let context = canvas.getContext("2d")
     fillBackgroundColor(canvas, context)
+    // drawText(canvas, context)
     
     let selectObj = document.getElementById("shape")
     let index = selectObj.selectedIndex
@@ -20,6 +21,8 @@ function previewHandler() {
             drawCircle(canvas, context)
         }
     }
+    drawText(canvas, context)
+    drawBird(canvas, context)
 }
 
 function drawSquares(canvas, context) {
@@ -75,4 +78,69 @@ function drawCircle(canvas, context) {
 
     context.fillStyle = "lightblue"
     context.fill()
+}
+
+// function updateTweets(tweets) {
+//     let tweetsSelection = document.getElementById("tweets")
+//     for (let i=0; i < tweets.length; i++) {
+//         tweet = tweets[i]
+//         let option = document.createElement("option")
+//         option.text = tweet.text
+//         option.value = tweet.text.replace("\"", "'")
+//         tweetsSelection.options.add(option)
+//     }
+//     tweetsSelection.selectedIndex = 0
+// }
+
+function updateTweets(tweets) {
+	var tweetsSelection = document.getElementById("tweets");
+
+	// add all tweets to the tweets menu
+	for (var i = 0; i < tweets.length; i++) {
+		tweet = tweets[i];
+
+		// create option
+		var option = document.createElement("option");
+		option.text = tweet.text;
+
+		// strip any quotes out of the tweet so they don't mess up our option
+		option.value = tweet.text.replace("\"", "'");
+
+		// add option to select
+		tweetsSelection.options.add(option);
+    }
+	// make sure the top tweet is selected
+	tweetsSelection.selectedIndex = 0;
+}
+
+function drawText(canvas, context) {
+	var selectObj = document.getElementById("foregroundColor");
+	var index = selectObj.selectedIndex;
+	var fgColor = selectObj[index].value;
+
+	context.fillStyle = fgColor;
+	context.font = "bold 1em sans-serif";
+	context.textAlign = "left";
+	context.fillText("Я повелся на этот твит", 20, 40);
+
+
+	// draw the tweet!
+	// selectObj = document.getElementById("tweets");
+	// index = selectObj.selectedIndex;
+	var tweet = "selectObj[index].value";
+	context.font = "italic 1.2em serif";
+	context.fillText(tweet, 150, 100);
+
+    context.font = "bold 1em sans-serif"
+    context.textAlign = "right"
+    context.fillText("... а в результате получил эту паршивую майку!", 580, 180)
+}
+
+function drawBird(canvas, context) {
+	var twitterBird = new Image();
+	twitterBird.src = "twitterBird.png";
+	twitterBird.onload = function() {
+		context.drawImage(twitterBird, 20, 120, 70, 70);
+	};
+
 }
